8739fe1148b86deb8b970c9b57d11f20107b83b3
Abhinand K <abhinandkofficial01@gmail.com>
2026-08-12 23:46:01 +0530

    Error resolved

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,7 +14,7 @@ export default function App() {
       <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Go to Onboarding</Link>
       <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go to Sign In</Link>
       <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go to Sign Up</Link>
-      <Link href="/subscriptions/spotify">Spotify Subscription</Link>
+      <Link href={{ pathname: "/subscriptions/[id]", params: { id: "spotify" } }}>Spotify Subscription</Link>
       <Link href={{ pathname: "/subscriptions/[id]", params: { id: "claude" } }}>Claude Max Scuscrition</Link>
     </SafeAreaView>
   );
