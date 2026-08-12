0d11d7aebd32457a4c2234ea23ba0a6d2210da0e
Abhinand K <abhinandkofficial01@gmail.com>
2026-08-12 23:59:03 +0530

    Fix added

--- image.d.ts
+++ image.d.ts
@@ -1,13 +1,15 @@
+import { ImageSourcePropType } from "react-native";
+
 declare module "*.png" {
-    const value: any;
+    const value: ImageSourcePropType;
     export default value;
 }
 declare module "*.jpg" {
-    const value: any;
+    const value: ImageSourcePropType;
     export default value;
 }
 declare module "*.jpeg" {
-    const value: any;
+    const value: ImageSourcePropType;
     export default value;
 }
 declare module "*.svg" {
@@ -15,6 +17,6 @@ declare module "*.svg" {
     export default value;
 }
 declare module "*.gif" {
-    const value: any;
+    const value: ImageSourcePropType;
     export default value;
 }
